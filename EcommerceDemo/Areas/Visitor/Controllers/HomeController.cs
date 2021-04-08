1b965e25e89c4ca2720c997f95c2c079426c7004
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2021-04-08 22:54:54 +0600

    Product Table is Dynamic (80%)

--- EcommerceDemo/Areas/Visitor/Controllers/HomeController.cs
+++ EcommerceDemo/Areas/Visitor/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace EcommerceDemo.Controllers
     public class HomeController : Controller
     {
         private ApplicationDbContext _db;
+        //private Context _context;
 
         public HomeController(ApplicationDbContext db)
         {
@@ -39,10 +40,30 @@ namespace EcommerceDemo.Controllers
         }
 
         public IActionResult Details(int id)
-        {
-            var data = from p in _db.Products join pc in _db.ProductCatagories on p.catagory_id equals pc.id where p.id == id select new { p , pc };
-            Console.WriteLine(data.ToList());
-            return View();
+        {            
+            ProductJoinCatagory productDetails = new ProductJoinCatagory();
+    
+            var bara = _db.Products.Join(_db.ProductCatagories, product => product.catagory_id , productCat => productCat.id, (product, productCat) 
+                => new { product_id = product.id, catagory_name = productCat.catagory_name, product_name = product.product_name , 
+                    product_details = product.product_description, catagory_policy = productCat.policy, product_image = product.product_img, 
+                    video_url = product.video_url, packing_type = product.packing_type, product_material = product.product_material, 
+                    product_brand = product.product_brand, product_price = product.product_price, produc_sell = product.product_sell, minimum_order = product.minimum_order } ).FirstOrDefault();
+
+            productDetails.product_id = bara.product_id;
+            productDetails.product_name = bara.product_name;
+            productDetails.catagory_name = bara.catagory_name;
+            productDetails.product_details = bara.product_details;
+            productDetails.catagory_policy = bara.catagory_policy;
+            productDetails.product_image = bara.product_image;
+            productDetails.video_url = bara.video_url;
+            productDetails.packing_type = bara.packing_type;
+            productDetails.product_material = bara.product_material;
+            productDetails.product_brand = bara.product_brand;
+            productDetails.product_price = bara.product_price;
+            productDetails.produc_sell = bara.produc_sell;
+            productDetails.minimum_order = bara.minimum_order;
+            //System.Diagnostics.Debug.WriteLine(data.ToString());
+            return View(productDetails);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ EcommerceDemo/Models/ProductJoinCatagory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceDemo.Models
+{
+    public class ProductJoinCatagory
+    {
+        public int product_id { get; set; }
+        public String catagory_name { get; set; }
+        public String product_name { get; set; }
+        public String product_details { get; set; }
+        public String catagory_policy { get; set; }
+        public String product_image { get; set; }
+        public String video_url { get; set; }
+        public String packing_type { get; set; }
+        public String product_material { get; set; }
+        public String product_brand { get; set; }
+        public String product_price { get; set; }
+        public String produc_sell { get; set; }
+        public String minimum_order { get; set; }
+
+    }
+}
