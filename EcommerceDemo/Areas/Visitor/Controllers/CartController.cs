82d3aae134ea8fea8d056fbed19b996585cf24d6
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2021-05-13 02:59:34 +0600

    Cart on progress with the help of LocalStorage(JS)

---
+++ EcommerceDemo/Areas/Visitor/Controllers/CartController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceDemo.Models;
+using EcommerceDemo.Data;
+
+namespace EcommerceDemo.Areas.Visitor.Controllers
+{
+    [Area("Visitor")]
+    public class CartController : Controller
+    {
+        private ApplicationDbContext _db;
+        //private Context _context;
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public dynamic GetProductDetails(string id)
+        {
+            System.Diagnostics.Debug.WriteLine("................" + id);
+            string somevalue = id;
+            var products = _db.Products.Where(p => somevalue.Contains(p.id.ToString())).ToList();
+            return products;
+        }
+    }
+}
