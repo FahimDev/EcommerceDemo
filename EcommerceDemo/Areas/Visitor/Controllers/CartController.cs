8d2ec355980422b8e2ae0b59cb5ada1e4bf1ec6d
Md.Antonin Islam <md.antonin6862@gmail.com>
2021-05-22 22:12:19 +0600

    cart working

--- EcommerceDemo/Areas/Visitor/Controllers/CartController.cs
+++ EcommerceDemo/Areas/Visitor/Controllers/CartController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using EcommerceDemo.Models;
 using EcommerceDemo.Data;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Dynamic;
 
 namespace EcommerceDemo.Areas.Visitor.Controllers
 {
@@ -13,7 +16,15 @@ namespace EcommerceDemo.Areas.Visitor.Controllers
     {
         private ApplicationDbContext _db;
         //private Context _context;
-
+        public class IJsonType
+        {
+            public int id { get; set; }
+            public int quantity { get; set; }
+        }
+        public class ProductsExtend : Products
+        {
+            public int quantity { get; set; }
+        }
         public CartController(ApplicationDbContext db)
         {
             _db = db;
@@ -21,17 +32,35 @@ namespace EcommerceDemo.Areas.Visitor.Controllers
 
         public dynamic GetProductDetails(string id)
         {
-            System.Diagnostics.Debug.WriteLine("................" + id);
-
-            //string[] somevalue = id.Split(',');
-            //List<dynamic> products = new List<dynamic>();
-            //foreach (var item in somevalue)
-            //{
-            //    var product = _db.Products.Where(p => p.id == int.Parse(item)).FirstOrDefault();
-            //    products.Add(product);
-            //}
             
-            return id;
+
+            System.Diagnostics.Debug.WriteLine("................" + id);
+            var jobj = JsonConvert.DeserializeObject<IEnumerable<IJsonType>>(id);
+            int total = 0;
+            List<ProductsExtend> products = new List<ProductsExtend>();
+            foreach (var item in jobj)
+            {
+                Products product =  _db.Products.Where(p => p.id == item.id).FirstOrDefault();
+                ProductsExtend pe = new ProductsExtend
+                {
+                    id = product.id,
+                    product_img = product.product_img,
+                    product_name = product.product_name,
+                    product_price = product.product_price,
+                    quantity = item.quantity,
+                };
+
+                total += product.product_price * item.quantity;
+                
+                products.Add(pe);
+                //System.Diagnostics.Debug.WriteLine(item.id);
+            }
+
+            dynamic cartDetails = new ExpandoObject();
+            cartDetails.products = products;
+            cartDetails.total = total;
+
+            return cartDetails;
         }
     }
 }
