07420ab1aa867f5a267f16bd216fd63aaa420799
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2021-05-24 22:55:30 +0600

    View Cart on full window is DONE

---
+++ EcommerceDemo/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using EcommerceDemo.Data;
+
+namespace EcommerceDemo.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
